8e2136ddeb11fd793ad88b8902d47055414a51b8
Consultor Seidor 1 <SEIDOR1@capsa.es>
2019-11-28 14:17:32 +0100

    cambio de tabla a lista

--- page1/src/compPage1.js
+++ page1/src/compPage1.js
@@ -1,9 +1,14 @@
 import { html, render } from "lit-html";
 
-import "@ui5/webcomponents/dist/Table.js";
-import "@ui5/webcomponents/dist/TableColumn.js";
-import "@ui5/webcomponents/dist/TableRow.js";
-import "@ui5/webcomponents/dist/TableCell.js";
+// import "@ui5/webcomponents/dist/Table.js";
+// import "@ui5/webcomponents/dist/TableColumn.js";
+// import "@ui5/webcomponents/dist/TableRow.js";
+// import "@ui5/webcomponents/dist/TableCell.js";
+
+import "@ui5/webcomponents/dist/List.js";
+import "@ui5/webcomponents/dist/StandardListItem.js";
+import "@ui5/webcomponents/dist/CustomListItem.js";
+import "@ui5/webcomponents/dist/GroupHeaderListItem.js";
 
 export class compPage1 extends HTMLElement {
   set datos_lista(datos) {
@@ -18,77 +23,42 @@ export class compPage1 extends HTMLElement {
 
   connectedCallback() {
     const template = html`
-
-      <ui5-table
-       
-        id="table"
-        no-data-text="No Data"
-        show-no-data
-      >
-        <!-- Columns -->
-        <ui5-table-column slot="columns" first>
-          <span style="line-height: 1.4rem">Quanto</span>
-        </ui5-table-column>
-
-        <ui5-table-column slot="columns" popin-text="Tipo de almacén">
-          <span style="line-height: 1.4rem">Tipo Alm.</span>
-        </ui5-table-column>
-
-        <ui5-table-column slot="columns" popin-text="Ubicación" 
-          <span style="line-height: 1.4rem">Ubic.</span>
-        </ui5-table-column>
-
-        <ui5-table-column slot="columns" popin-text="Nº material" >
-          <span style="line-height: 1.4rem">Mat.</span>
-        </ui5-table-column>
-
-        <ui5-table-column slot="columns" popin-text="Descripción" min-width="700">
-        <span style="line-height: 1.4rem">Descripción</span>
-        </ui5-table-column>
-
-        <ui5-table-column slot="columns" popin-text="Cantidad">
-        <span style="line-height: 1.4rem">Cantidad</span>
-        </ui5-table-column>
-
-        <ui5-table-column slot="columns" popin-text="Unidad" last>
-        <span style="line-height: 1.4rem">Unidad</span>
-        </ui5-table-column>
-
-        ${this.get_rows()}
-      </ui5-table>
+    <ui5-list id="myList" class="full-width">
+      <ui5-li icon="sap-icon://nutrition-activity" description="Tropical plant with an edible fruit" info="In-stock" info-state="Success">Pineapple</ui5-li>
+      <ui5-li icon="sap-icon://nutrition-activity" description="Occurs between red and yellow" info="Expires" info-state="Warning">Orange</ui5-li>
+      <ui5-li icon="sap-icon://nutrition-activity" description="The yellow lengthy fruit" info="Re-stock" info-state="Error">Banana</ui5-li>
+      <ui5-li icon="sap-icon://nutrition-activity" description="The tropical stone fruit" info="Re-stock" info-state="Error">Mango</ui5-li>
+    </ui5-list>
+      
     `;
 
     render(template, this);
   }
 
+  get_rows() {
+    return this.state.map(
+      el => html`
+
+      `
+    );
+  }
+
+
   // get_rows() {
   //   return this.state.map(
   //     el => html`
   //       <ui5-table-row>
-  //         <ui5-table-cell>${el.id}</ui5-table-cell>
-  //         <ui5-table-cell>${el.name}</ui5-table-cell>
-  //         <ui5-table-cell>${el.email}</ui5-table-cell>
-  //         <ui5-table-cell>${el.body}</ui5-table-cell>
+  //         <ui5-table-cell>${el.LQNUM}</ui5-table-cell>
+  //         <ui5-table-cell>${el.LGTYP}</ui5-table-cell>
+  //         <ui5-table-cell>${el.LGPLA}</ui5-table-cell>
+  //         <ui5-table-cell>${el.MATNR}</ui5-table-cell>
+  //         <ui5-table-cell>${el.MAKTX}</ui5-table-cell>
+  //         <ui5-table-cell>${el.VERME}</ui5-table-cell>
+  //         <ui5-table-cell>${el.MEINS}</ui5-table-cell>
   //       </ui5-table-row>
   //     `
   //   );
   // }
-
-  get_rows() {
-    return this.state.map(
-      el => html`
-        <ui5-table-row>
-          <ui5-table-cell>${el.LQNUM}</ui5-table-cell>
-          <ui5-table-cell>${el.LGTYP}</ui5-table-cell>
-          <ui5-table-cell>${el.LGPLA}</ui5-table-cell>
-          <ui5-table-cell>${el.MATNR}</ui5-table-cell>
-          <ui5-table-cell>${el.MAKTX}</ui5-table-cell>
-          <ui5-table-cell>${el.VERME}</ui5-table-cell>
-          <ui5-table-cell>${el.MEINS}</ui5-table-cell>
-        </ui5-table-row>
-      `
-    );
-  }
 
 
 
